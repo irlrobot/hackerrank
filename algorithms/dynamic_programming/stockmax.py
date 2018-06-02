0200a755e649a3efdf4182659f302f363b46b5ff
Josh Campbell <josh@userdel.com>
2018-06-02 14:44:48 -0700

    getting there

--- algorithms/dynamic_programming/stockmax.py
+++ algorithms/dynamic_programming/stockmax.py
@@ -1,1 +1,44 @@
+#!/bin/python3
 # https://www.hackerrank.com/challenges/stockmax/problem
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the stockmax function below.
+def stockmax(prices):
+    profit = 0
+    highest_price = max(prices)
+    purchases = []
+
+    for price in prices:
+        if price < highest_price:
+            purchases.append(price)
+            print(purchases)
+            profit -= price
+            print(profit)
+        if price == highest_price:
+            for index, purchase in enumerate(purchases):
+                profit += purchase
+                del purchases[index]
+
+    return profit
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        prices = list(map(int, input().rstrip().split()))
+
+        result = stockmax(prices)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
