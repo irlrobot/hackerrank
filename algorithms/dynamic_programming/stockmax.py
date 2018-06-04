ebfc2c5bd1ac4c91a148031aa419f344da94bf44
Josh Campbell <josh@userdel.com>
2018-06-03 20:00:11 -0700

    removes debugging and improves efficiency

--- algorithms/dynamic_programming/stockmax.py
+++ algorithms/dynamic_programming/stockmax.py
@@ -1,10 +1,6 @@
 #!/bin/python3
 # https://www.hackerrank.com/challenges/stockmax/problem
-import math
 import os
-import random
-import re
-import sys
 
 
 # Complete the stockmax function below.
@@ -16,40 +12,31 @@ def stockmax(prices):
 
     for index, price in enumerate(prices):
         print(("Pass {} of {}").format(index + 1, len(prices)))
-        # print(prices)
-        # print(profit)
-        # print(purchases)
-        # print(remaining_prices)
-        # print(price)
-        # print(highest_price)
         if index == len(prices) - 1:
-            # print("Last Price!")
             if price == highest_price:
-                # print("Highest price found...")
                 for _purchase in purchases:
                     print("Selling a share...")
                     profit += price
                     print(profit)
                 print("Done selling shares...")
         else:
-            # print("Not last price...")
             if price == highest_price:
-                # print("Highest price found...")
                 for _purchase in purchases:
                     print("Selling a share...")
                     profit += price
                     print(profit)
                 purchases = []
-                print("Done selling shares...")
 
-            del remaining_prices[0]
-            if any(i > price for i in remaining_prices):
-                print("Buying a share...")
-                purchases.append(price)
-                profit -= price
-            # print(("remaining prices {}").format(remaining_prices))
-            highest_price = max(remaining_prices)
-            # print(("Highest price set to {}").format(highest_price))
+                del remaining_prices[0]
+                highest_price = max(remaining_prices)
+            else:
+                del remaining_prices[0]
+                highest_price = max(remaining_prices)
+
+                if price < highest_price:
+                    print("Buying a share...")
+                    purchases.append(price)
+                    profit -= price
 
     return profit
 
