b46c39338b9fe8241486d6f906cbc182e804fc64
Josh Campbell <josh@userdel.com>
2018-06-03 19:31:06 -0700

    lookin good

--- algorithms/dynamic_programming/stockmax.py
+++ algorithms/dynamic_programming/stockmax.py
@@ -10,19 +10,46 @@ import sys
 # Complete the stockmax function below.
 def stockmax(prices):
     profit = 0
-    highest_price = max(prices)
     purchases = []
+    remaining_prices = prices[:]
+    highest_price = max(prices)
 
-    for price in prices:
-        if price < highest_price:
-            purchases.append(price)
-            print(purchases)
-            profit -= price
-            print(profit)
-        if price == highest_price:
-            for index, purchase in enumerate(purchases):
-                profit += purchase
-                del purchases[index]
+    for index, price in enumerate(prices):
+        print(("Pass {} of {}").format(index + 1, len(prices)))
+        # print(prices)
+        # print(profit)
+        # print(purchases)
+        # print(remaining_prices)
+        # print(price)
+        # print(highest_price)
+        if index == len(prices) - 1:
+            # print("Last Price!")
+            if price == highest_price:
+                # print("Highest price found...")
+                for _purchase in purchases:
+                    print("Selling a share...")
+                    profit += price
+                    print(profit)
+                print("Done selling shares...")
+        else:
+            # print("Not last price...")
+            if price == highest_price:
+                # print("Highest price found...")
+                for _purchase in purchases:
+                    print("Selling a share...")
+                    profit += price
+                    print(profit)
+                purchases = []
+                print("Done selling shares...")
+
+            del remaining_prices[0]
+            if any(i > price for i in remaining_prices):
+                print("Buying a share...")
+                purchases.append(price)
+                profit -= price
+            # print(("remaining prices {}").format(remaining_prices))
+            highest_price = max(remaining_prices)
+            # print(("Highest price set to {}").format(highest_price))
 
     return profit
 
